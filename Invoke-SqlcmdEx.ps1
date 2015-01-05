69fe2f17cd8606e8e6cdbee138662e690dce3112
Michael Naumov <michael.naumov@volia-software.com>
2015-01-06 00:45:52 +0200

    Add boilerplate

---
+++ Invoke-SqlcmdEx.ps1
@@ -0,0 +1,18 @@
+#requires -version 2.0
+
+param
+(
+)
+
+$script:ErrorActionPreference = "Stop"
+Set-StrictMode -Version Latest
+function PSScriptRoot { $MyInvocation.ScriptName | Split-Path }
+
+trap { throw $Error[0] }
+
+function Main
+{
+
+}
+
+Main
