10927db693939831aa4a3b5523d0222c893f1156
Michael Naumov <michael.naumov@volia-software.com>
2015-01-06 09:35:02 +0200

    Implement Get-Batches

--- Invoke-SqlcmdEx.ps1
+++ Invoke-SqlcmdEx.ps1
@@ -23,11 +23,70 @@ function Main
     try
     {
         $connection.Open()
+        
+        $batches = Get-Batches -Path $InputFile
+        
+        foreach ($batch in $batches)
+        {
+            Execute-Batch -Connection $connection -Batch $batch
+        }
     }
     finally
     {
         $connection.Dispose()
     }
+}
+
+function Get-Batches
+{
+    param
+    (
+        [string] $Path
+    )
+    
+    $scriptLines = @(Get-Content -Path $Path)
+    $scriptLines += "GO" # Add trailing GO
+    $batches = @()
+    $batchLines = @()
+    $offset = 0
+    for ($i = 0; $i -lt $scriptLines.Length; $i++)
+    {
+        $scriptLine = $scriptLines[$i]
+        if ($scriptLine -match "\s*GO\s*")
+        {
+            $query = $batchLines -join "`r`n"
+            if ($query -notmatch "^\s*$")
+            {
+                $batches += New-Object -TypeName PSObject -Property `
+                @{
+                    Query = $query;
+                    Offset = $offset;
+                }
+            }
+
+            $offset = $i + 1
+            $batchLines = @()
+        }
+        else
+        {
+            $batchLines += $scriptLine
+        }
+    }
+    
+    $batches
+}
+
+function Execute-Batch
+{
+    param
+    (
+        [System.Data.SqlClient.SqlConnection] $Connection,
+        [PSObject] $Batch
+    )
+    
+    $command = $Connection.CreateCommand()
+    $command.CommandText = $Batch.Query
+    $command.ExecuteNonQuery()
 }
 
 Main
